5265e0d7d179bb3135a76fe8e3f2eae19a44702c
Kleinloog <Carlo.Kleinloog@bis-is.com>
2023-09-11 10:41:20 +0200

    changes

--- CheckMK/Oxidized/src/base/plugins/agent_based/oxidized_status.py
+++ CheckMK/Oxidized/src/base/plugins/agent_based/oxidized_status.py
@@ -3,7 +3,6 @@
 import time
 import json
 from datetime import datetime, timedelta
-from cmk.base.check_api import get_bytes_human_readable, get_percent_human_readable
 from .agent_based_api.v1 import (
     register,
     Service,
@@ -12,23 +11,30 @@ from .agent_based_api.v1 import (
     Metric,
     render,
 )
+
+THRESHOLD = timedelta(days=1)
+
 def parse_oxidized_status(string_table):
     section = {}
     for row in string_table:
-        (item, status, time, ip, group, model)  = row
+        (item, status, backup_date, backup_time, ip_address, group, model, start_date, start_time, end_date, end_time)  = row
 
         try:
             status:str=status
         except ValueError:
             status=0
         try:
-            time:str=time
+            backup_date:str=backup_date
         except ValueError:
-            time=0
+            backup_date=0
         try:
-            ip:str=ip
+            backup_time:str=backup_time
         except ValueError:
-            ip=0
+            backup_time=0
+        try:
+            ip_address:str=ip_address
+        except ValueError:
+            ip_address=0
         try:
             group:str=group
         except ValueError:
@@ -37,13 +43,34 @@ def parse_oxidized_status(string_table):
             model:str=model
         except ValueError:
             model=0
+        try:
+            start_date:str=start_date
+        except ValueError:
+            start_date=0
+        try:
+            start_time:str=start_time
+        except ValueError:
+            start_time=0
+        try:
+            end_date:str=end_date
+        except ValueError:
+            end_date=0
+        try:
+            end_time:str=end_time
+        except ValueError:
+            end_time=0
         section[item] = {
 
 'status' : status,
-'time' : time,
-'ip' : ip,
+'backup_date' : backup_date,
+'backup_time' : backup_time,
+'ip_address' : ip_address,
 'group' : group,
 'model' : model,
+'start_date' : start_date,
+'start_time' : start_time,
+'end_date' : end_date,
+'end_time' : end_time,
         }
     return section
 
@@ -57,26 +84,34 @@ def discovery_oxidized_status(section):
         yield Service(item=item)
 
 def check_oxidized_status(item, section):
-    
+
     data = section[item]
     status:str = data['status']
-    time:str = data['time']
-    ip:str = data['ip']
+    backup_date:str = data['backup_date']
+    backup_time:str = data['backup_time']
+    ip_address:str = data['ip_address']
     group:str = data['group']
     model :str= data['model']
-
-
-    if item in section.keys():
-        yield Result(state=State.OK, 
-                    summary=f"Last backup at {(time)} was successful",
-                    details = f"Last Update: {(time)} \n \
-                    Group: {(group)}, Model: {model}, IP Address: {ip}",
+    start_date :str= data['start_date']
+    start_time :str= data['start_time']
+    end_date :str= data['end_date']
+    end_time :str= data['end_time']
+    if status == 'never':
+        yield Result(state=State.UNKNOWN, 
+                    summary=f"No backup for {item}",
+                    )
+    if status == 'no_connection':
+        yield Result(state=State.CRIT,
+                    summary=f"Last backup at {(start_date)} {(start_time)}, ended {(end_date)} {(end_time)} at was not successful",
+                    details = f"Last Update: {(backup_date)} {(backup_time)} \n \
+                    Group: {(group)}, Model: {model}, IP Address: {ip_address}",
                     )
     else:
-        yield Result(state=State.CRIT,
-                    summary=f"Last backup at {(time)} was not successful",
-                    details = f"Last Update: {(time)} \n \
-                    Group: {(group)}, Model: {model}, IP Address: {ip}",
+        if status == 'success':
+            yield Result(state=State.OK,
+                    summary=f"Last backup at {(start_date)} {(start_time)}, ended {(end_date)} {(end_time)} was successful",
+                    details = f"Last Update: {(backup_date)} {(backup_time)} \n \
+                    Group: {(group)}, Model: {model}, IP Address: {ip_address}",
                     )
 
 register.check_plugin(
